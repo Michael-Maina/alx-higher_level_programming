32b7eff4ef3c88d7aeeb52987f157754a9532564
Michael-Maina <michaelmaina02@gmail.com>
2022-12-03 17:49:10 +0300

    Style and bug fix on 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,17 +3,19 @@ def max_integer(my_list=[]):
     i = len(my_list) - 1
     new_list = my_list.copy()
 
-    while i > 1:
-        j = 0
+    if my_list:
+        while i > 1:
+            j = 0
 
-        while j < i:
-            if new_list[j] > new_list[j + 1]:
-                temp = new_list[j]
-                new_list[j] = new_list[j + 1]
-                new_list[j + 1] = temp
+            while j < i:
+                if new_list[j] > new_list[j + 1]:
+                    temp = new_list[j]
+                    new_list[j] = new_list[j + 1]
+                    new_list[j + 1] = temp
 
-            j += 1
+                    j += 1
 
-        i-= 1
+                    i -= 1
 
-    return new_list[-1]
+                    return new_list[-1]
+    return None
