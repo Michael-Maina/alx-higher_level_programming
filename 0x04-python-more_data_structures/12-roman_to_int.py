0b7204d20524d70b3febb484d61423110fd75de3
Michael-Maina <michaelmaina02@gmail.com>
2023-01-01 14:41:37 +0300

    Pycodestyle edit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
         return 0
     total = 0
     prev_val = 0
-    ref = {'I': 1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    ref = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in range(len(roman_string)):
         if prev_val < ref[roman_string[i]]:
             total -= prev_val * 2
