90e86c9be658420262c80ae87b4ea6536da48a94
Michael-Maina <michaelmaina02@gmail.com>
2023-01-05 12:02:24 +0300

    Error handling

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,30 +9,30 @@ class Rectangle:
         self.height = height
         self.width = width
 
-        @property
-        def width(self):
-            """getter function for the private instance attribute width"""
-            return self.__width
+    @property
+    def width(self):
+        """getter function for the private instance attribute width"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """setter function for the private instance attribute width"""
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """setter function for the private instance attribute width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """getter function for the private instance attribute height"""
-            return self.__height
+    @property
+    def height(self):
+        """getter function for the private instance attribute height"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """setter function for the private instance attribute height"""
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """setter function for the private instance attribute height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
