dd4ee9f4a16c75d832b4ec1dbe6996a2d0342fd0
Michael-Maina <michaelmaina02@gmail.com>
2023-01-05 11:28:09 +0300

    Error handling

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
             if not isinstance(value, int):
                 raise TypeError("width must be an integer")
             if value < 0:
-                raise ValueError("value must be >= 0")
+                raise ValueError("width must be >= 0")
             self.__width = value
 
         def __init__(self, width=0, height=0):
@@ -39,5 +39,5 @@ class Rectangle:
             if not isinstance(value, int):
                 raise TypeError("height must be an integer")
             if value < 0:
-                raise ValueError("value must be >= 0")
+                raise ValueError("height must be >= 0")
             self.__height = value
