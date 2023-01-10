8efb3ba35c33ac4cd99523af298a93da83fc8618
Michael-Maina <michaelmaina02@gmail.com>
2023-01-10 18:02:28 +0300

    Updated 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,7 +2,7 @@
 """ Class to JSON Module """
 
 
-def load_from_json_file(filename):
+def class_to_json(obj):
     """ Function returns the dictionary description with simple data structure
     (list, dictionary, string, integer and boolean)
     for JSON serialization of an object """
