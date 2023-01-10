5395be130f6f41f7aa34442ed5d525392933ce8d
Michael-Maina <michaelmaina02@gmail.com>
2023-01-10 17:03:49 +0300

    Added 7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Add Arguments to Python List Module """
+
+
+if __name__ == "__main__":
+    from sys import argv
+    save_to = __import__('5-save_to_json_file').save_to_json_file
+    load_from = __import__('6-load_from_json_file').load_from_json_file
+
+    args = []
+    try:
+        args = load_from("add_item.json")
+        for elem in argv[1:]:
+            args.append(elem)
+        save_to(args, "add_item.json")
+    except:
+        for elem in argv[1:]:
+            args.append(elem)
+        save_to(args, "add_item.json")
