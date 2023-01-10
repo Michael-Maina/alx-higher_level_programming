cf0ba02583a4f2462be3280bd24e0c32d09db98a
Michael-Maina <michaelmaina02@gmail.com>
2023-01-10 12:04:43 +0300

    Style edit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" Read File Module """
+
+
 def read_file(filename=""):
+    """ Function reads a text file(UTF-8) and prints it to stdout """
     with open("my_file_0.txt", mode="r", encoding="utf-8") as my_file:
         print(my_file.read())
