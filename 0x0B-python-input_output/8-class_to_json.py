14e9c594ee897ba7752f687eab648de1c1f467d7
Michael-Maina <michaelmaina02@gmail.com>
2023-01-10 17:59:28 +0300

    Error handling

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Class to JSON Module """
+
+
+def load_from_json_file(filename):
+    """ Function returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object """
+    return obj.__dict__
