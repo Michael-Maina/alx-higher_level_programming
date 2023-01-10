55a873ed9824859e3b3850f3a1325ed0492a15cb
Michael-Maina <michaelmaina02@gmail.com>
2023-01-10 12:08:42 +0300

    Error handling

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """ Function reads a text file(UTF-8) and prints it to stdout """
     with open("my_file_0.txt", mode="r", encoding="utf-8") as my_file:
-        print(my_file.read())
+        print(my_file.read(), end='')
