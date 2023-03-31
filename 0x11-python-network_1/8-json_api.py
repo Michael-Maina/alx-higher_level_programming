ca3f2c09e68f4af762e3aefce9958bb00f3521c8
Michael-Maina <michaelmaina02@gmail.com>
2023-03-31 18:04:30 +0300

    Error handling

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    if len(argv[1]) == 1:
+    if len(argv) == 1:
         data = {"q": ""}
     elif not isinstance(argv[1], str):
         print("No result")
