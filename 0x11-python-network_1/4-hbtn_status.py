1b306a8d98c1f9ce996051ade74aab432f3c5b2b
Michael-Maina <michaelmaina02@gmail.com>
2023-03-31 14:58:12 +0300

    Style fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 
 if __name__ == "__main__":
     req = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(req.text),
-                                                                 req.text))
+    print("Body response:\n\t- type: {}\n\t- \
+content: {}".format(type(req.text), req.text))
