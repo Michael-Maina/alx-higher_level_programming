de9adc03d9fce6431a94db05089cda17ca8f8465
Michael-Maina <michaelmaina02@gmail.com>
2023-03-31 13:15:40 +0300

    Style editing

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 script takes in a URL and an email, sends a POST request to the passed URL with
-the email as a parameter, and displays the body of the response decoded in utf-8
+email as a parameter, and displays the body of the response decoded in utf-8
 """
 from urllib.request import urlopen, Request
 from urllib import parse
